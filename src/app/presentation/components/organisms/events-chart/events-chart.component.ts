11784081565d4ebd0d35c759a4bb2149a585e58a
Pedro Junior <49008209+PedruJr@users.noreply.github.com>
2025-05-08 14:42:37 -0300

    styles: styles update

--- src/app/presentation/components/organisms/events-chart/events-chart.component.ts
+++ src/app/presentation/components/organisms/events-chart/events-chart.component.ts
@@ -35,50 +35,87 @@ export class EventsChartComponent implements OnChanges {
         axisPointer: { type: 'shadow' }
       },
       legend: {
-        bottom: 0
+        bottom: 0,
+        itemGap: 40,
+        itemWidth: 16,
+        itemHeight: 16,
+        symbol: 'rect',
+        textStyle: {
+          fontSize: 12,
+          color: '#000',
+          fontWeight: 500
+        },
+        data: ['Encontros', 'Mensagens', 'Checkpoints', 'Exploração']
       },
       grid: {
-        left: '3%',
+        left: '6%',
         right: '4%',
-        bottom: '10%',
+        bottom: '20%',
         containLabel: true
       },
       xAxis: {
         type: 'category',
         data: this.data.map(d => d.day),
-        axisTick: { alignWithLabel: true }
+        boundaryGap: true,
+        axisTick: { alignWithLabel: true },
+        axisLabel: {
+          align: 'center',
+          interval: 0
+        },
+        offset: 12
       },
       yAxis: {
         type: 'value',
-        name: 'Quantidade de Eventos'
+        name: 'Quantidade de Eventos',
+        max: 200,
+        interval: 50,
+        nameRotate: 90,
+        nameLocation: 'center',
+        nameGap: 50,
+        nameTextStyle: {
+          fontWeight: 500,
+          fontSize: 14
+        },
+        axisLabel: {
+          fontWeight: 600
+        }
       },
+      color: ['#4CAF50', '#616161', '#00BCD4', '#7E57C2'],
       series: [
         {
           name: 'Encontros',
           type: 'bar',
           stack: 'total',
+          barWidth: '50%',
           emphasis: { focus: 'series' },
+          itemStyle: { opacity: 0.75 },
           data: this.data.map(d => d.encounters)
         },
         {
           name: 'Mensagens',
           type: 'bar',
           stack: 'total',
+          barWidth: '50%',
           emphasis: { focus: 'series' },
+          itemStyle: { opacity: 0.75 },
           data: this.data.map(d => d.messages)
         },
         {
           name: 'Checkpoints',
           type: 'bar',
           stack: 'total',
+          barWidth: '50%',
           emphasis: { focus: 'series' },
+          itemStyle: { opacity: 0.75 },
           data: this.data.map(d => d.checkpoints)
         },
         {
           name: 'Exploração',
           type: 'bar',
           stack: 'total',
+          barWidth: '50%',
           emphasis: { focus: 'series' },
+          itemStyle: { opacity: 0.75 },
           data: this.data.map(d => d.exploration)
         }
       ]
--- src/app/presentation/components/organisms/start-entities/start-entities.component.ts
+++ src/app/presentation/components/organisms/start-entities/start-entities.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
   selector: 'app-start-entities',
   templateUrl: './start-entities.component.html',
   styleUrls: ['./start-entities.component.scss'],
-  imports: [FormsModule]
+  imports: [FormsModule, MatIconModule]
 })
 export class StartEntitiesComponent {
   value = signal(1);
