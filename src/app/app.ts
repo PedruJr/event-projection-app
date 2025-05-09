b017b92aabdd6f642538b04a23c856d7ad335c34
Pedro Junior <49008209+PedruJr@users.noreply.github.com>
2025-05-09 18:12:34 -0300

    whim: splash screen meet

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,16 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import {EventProjectionComponent} from './presentation/pages/event-projection/event-projection.component';
 import {setCycles} from './core/signals/cycles.signal';
 import {mockCycles} from './core/mocks/mock-cycles';
+import {SplashScreenComponent} from './presentation/pages/splash-screen/splash-screen.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EventProjectionComponent],
+
+  imports: [ EventProjectionComponent, SplashScreenComponent, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
+  showSplash = true;
+
   ngOnInit(): void {
     setCycles(mockCycles);
+    setTimeout(() => {
+      this.showSplash = false;
+    }, 3000); // 3 segundos
   }
   title = 'event-projection-app';
 }
